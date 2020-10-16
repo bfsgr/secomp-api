522de9c49b911beeaaf9b8d692e5d173ce3cd671
bfsgr <bfsgr.code@protonmail.com>
2020-10-16 18:06:25 -0300

    Muda certos dados para ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,9 +47,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            ENV.fetch(),
-    password:             ENV.fetch(),
+    domain:               ENV.fetch('DOMINIO_EMAIL'),
+    user_name:            ENV.fetch('USUARIO'),
+    password:             ENV.fetch('SENHA_APP'),
     authentication:       'plain',
     enable_starttls_auto: true }
 end
