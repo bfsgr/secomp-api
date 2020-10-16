1a4209ef68c3f3065adcfe9576d10d071ef98a81
bfsgr <bfsgr.code@protonmail.com>
2020-10-16 17:58:23 -0300

    normaliza o endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/inscricao', to: 'inscrito#new'
+  post '/inscreve', to: 'inscrito#new'
   delete '/desinscreve', to: 'inscrito#delete',  as: 'desinscreve'
 end
